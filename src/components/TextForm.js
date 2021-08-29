c11ad253f4a6abbce020dbeb97b40db3e078e555
ayushk128 <54218436+ayushk128@users.noreply.github.com>
2021-08-29 23:01:38 +0530

    Some improvements and rectified about page

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,14 +31,14 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value = {text} onChange = {handleOnChange} style = {{backgroundColor: props.mode === 'dark'? 'grey' : 'white', color: props.mode === 'dark'? 'white' : 'black' }} id="myBox" rows="8"></textarea>
                 </div>
-                <div className="btn btn-primary mx-1" onClick = {handleUpClick}>Convert to UpperCase</div>
-                <div className="btn btn-primary mx-1" onClick = {handleLoClick}>Convert to LowerCase</div>
-                <div className="btn btn-primary mx-1" onClick = {handleClearClick}>Clear</div>
+                <div className="btn btn-primary mx-1 my-1" onClick = {handleUpClick}>Convert to UpperCase</div>
+                <div className="btn btn-primary mx-1 my-1" onClick = {handleLoClick}>Convert to LowerCase</div>
+                <div className="btn btn-primary mx-1 my-1" onClick = {handleClearClick}>Clear</div>
             </div>
             <div className="container my-3" style = {{color: props.mode === 'dark'? 'white' : 'black'}}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").length} Words and {text.length} Characters</p>
-                <p>{0.008 * text.split(" ").length} Minutes to read</p>
+                <p>{text.split(" ").filter((element) => {return element.length !== 0}).length} Words and {text.length} Characters</p>
+                <p>{0.008 * text.split(" ").filter((element) => {return element.length !== 0}).length} Minutes to read</p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Enter something above to preview here"}</p>
             </div>
